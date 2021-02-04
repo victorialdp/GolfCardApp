69827e6d614d1326bd396947d5d87fecc50210c7
Victoria Desktop <lopezdelpinmv@vcu.edu>
2021-02-04 02:31:54 -0500

    Began layout of second (menu) page

---
+++ app/src/main/java/com/testmvldp/golfcardapp/MenuPage.java
@@ -0,0 +1,14 @@
+package com.testmvldp.golfcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_page);
+    }
+}
