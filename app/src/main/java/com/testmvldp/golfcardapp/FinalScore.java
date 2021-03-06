b5d2375c27d6d77d35a3e663a47fea27f960f533
Derrick McGlone <mcgloneda@vcu.edu>
2021-03-05 22:37:27 -0500

    playerScreen Fix attempt

--- app/src/main/java/com/testmvldp/golfcardapp/FinalScore.java
+++ app/src/main/java/com/testmvldp/golfcardapp/FinalScore.java
@@ -2,9 +2,11 @@ package com.testmvldp.golfcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
-
+import android.widget.Button;
 import java.util.ArrayList;
 
 
@@ -42,6 +44,15 @@ public class FinalScore extends AppCompatActivity {
        TextView winner = (TextView) findViewById(R.id.winnerTextView);
        String winnerString = "Player " + playerOrder[0] + " is the winner!";
        winner.setText(winnerString);
+
+       Button again = (Button) findViewById(R.id.playAgainButton);
+       again.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent playerScreen = new Intent(view.getContext(), PlayersScreen.class);
+               startActivity(playerScreen);
+           }
+       });
 
     }
 }
