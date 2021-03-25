ba994b75d604d9c3b3dba4391aa4603e9d190038
katherine <reyesmartinkn@vcu.edu>
2021-03-24 23:32:46 -0400

    Updated final score page to include winner score

--- app/src/main/java/com/testmvldp/golfcardapp/FinalScore.java
+++ app/src/main/java/com/testmvldp/golfcardapp/FinalScore.java
@@ -2,11 +2,9 @@ package com.testmvldp.golfcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Button;
+
 import java.util.ArrayList;
 
 
@@ -39,20 +37,17 @@ public class FinalScore extends AppCompatActivity {
            }
            playerOrder[i] = minIndex + 1;
            scores.set(minIndex, 1000);
+
        }
 
        TextView winner = (TextView) findViewById(R.id.winnerTextView);
        String winnerString = "Player " + playerOrder[0] + " is the winner!";
        winner.setText(winnerString);
 
-       Button again = (Button) findViewById(R.id.playAgainButton);
-       again.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               Intent playerScreen = new Intent(view.getContext(), PlayersScreen.class);
-               startActivity(playerScreen);
-           }
-       });
+        TextView winnerScore = (TextView) findViewById(R.id.totalamt);
+        String winscore = " " + scores.get(0) + " ";
+        winnerScore.setText(winscore);
+
 
     }
 }
