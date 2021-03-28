254b26bfa0a76274e67c0c941f5f18ddd3847bed
Tanya <tara20152@gmail.com>
2021-03-28 16:19:33 -0400

    Added page to edit players.

---
+++ app/src/main/java/com/testmvldp/golfcardapp/EditPlayers.java
@@ -0,0 +1,14 @@
+package com.testmvldp.golfcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditPlayers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_players);
+    }
+}
